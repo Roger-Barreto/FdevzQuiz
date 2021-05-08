4ee064363e197cb6b4c58d109de18c22d79965cc
roger.barreto <roger.barreto@NOTEBOOK105>
2021-05-08 00:16:24 -0300

    first commit

---
+++ FDevQuiz/Controllers/userController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FDevQuiz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class userController : ControllerBase
+    {
+        public ActionResult getUser()
+        {
+            var user = JsonConvert.DeserializeObject(System.IO.File.ReadAllText(@"c:\JsonData\user.json"));
+
+            return Ok(user);
+        }
+    }
+}
